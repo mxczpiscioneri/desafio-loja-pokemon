41aaabca961e3eb0a841f5cb68e2c4e23a4075e3
Matheus Piscioneri <matheus.piscioneri@luizalabs.com>
2021-03-31 20:36:56 -0300

    fix: function format money

--- src/common/utils/format.js
+++ src/common/utils/format.js
@@ -1,5 +1,1 @@
-export const money = (value) => {
-  let formated = new Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(value)
-  formated = formated.replace(/\s/, ' ')
-  return formated
-}
+export const money = value => new Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(value)
